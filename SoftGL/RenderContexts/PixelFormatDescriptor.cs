dda63476b44eb305db8f8eaa0caca51f88b8c89e
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-10 16:06:47 +0800

    update

---
+++ SoftGL/RenderContexts/PixelFormatDescriptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SoftGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct PixelFormatDescriptor
+    {
+    }
+}
