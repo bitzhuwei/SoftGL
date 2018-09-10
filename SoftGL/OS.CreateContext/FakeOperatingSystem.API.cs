ce5a56751ebeed2b5b8d7bff6a30d7ad7e86dba0
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-10 12:02:04 +0800

    Call API from OS to SoftGLRenderContext.

---
+++ SoftGL/OS.CreateContext/FakeOperatingSystem.API.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SoftGL
+{
+    /// <summary>
+    /// Initialization component calls Operating System. Operating System calls Hardware Driver. Hardware Driver is actually SoftGL.
+    /// This is the 'opengl32.dll' in SoftGL environment.
+    /// </summary>
+    public partial class FakeOperatingSystem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="op"></param>
+        /// <param name="value"></param>
+        public static void glAccum(uint op, float value)
+        {
+            SoftGLRenderContext context = FakeOperatingSystem.GetCurrentContextObj();
+            if (context != null)
+            {
+                //context.Accum(op, value);
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ SoftGL/RenderContexts/SoftGLRenderContext.API.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        public void Accum(uint op, float value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
