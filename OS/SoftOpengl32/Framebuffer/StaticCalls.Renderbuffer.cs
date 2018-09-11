85687f4da2ae790b992e1431376cf15257591453
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-11 21:28:26 +0800

    impl glGenRenderbuffers and glBindRenderbuffer

---
+++ OS/SoftOpengl32/Framebuffer/StaticCalls.Renderbuffer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    /// <summary>
+    /// Initialization component calls Operating System. Operating System calls Hardware Driver. Hardware Driver is actually SoftGL.
+    /// This is the 'opengl32.dll' in SoftGL environment.
+    /// </summary>
+    public partial class StaticCalls
+    {
+        /// <summary>
+        /// generate renderbuffer object names.
+        /// </summary>
+        /// <param name="count">Specifies the number of renderbuffer object names to generate.</param>
+        /// <param name="names">Specifies an array in which the generated renderbuffer object names are stored.</param>
+        public static void glGenRenderbuffers(int count, uint[] names)
+        {
+            SoftGLRenderContext context = StaticCalls.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.GenRenderbuffers(count, names);
+            }
+        }
+
+        /// <summary>
+        /// bind a renderbuffer to a renderbuffer target.
+        /// </summary>
+        /// <param name="target">Specifies the renderbuffer target of the binding operation. target​ must be GL_RENDERBUFFER.</param>
+        /// <param name="name">Specifies the name of the renderbuffer object to bind.</param>
+        public static void glBindRenderbuffer(uint target, uint name)
+        {
+            SoftGLRenderContext context = StaticCalls.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.BindRenderbuffer(target, name);
+            }
+        }
+
+        /// <summary>
+        /// establish data storage, format and dimensions of a renderbuffer object's image.
+        /// </summary>
+        /// <param name="target">Specifies a binding to which the target of the allocation and must be GL_RENDERBUFFER.</param>
+        /// <param name="internalformat">Specifies the internal format to use for the renderbuffer object's image.</param>
+        /// <param name="width">Specifies the width of the renderbuffer, in pixels.</param>
+        /// <param name="height">Specifies the height of the renderbuffer, in pixels.</param>
+        public static void glRenderbufferStorage(uint target, uint internalformat, int width, int height)
+        {
+            SoftGLRenderContext context = StaticCalls.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.RenderbufferStorage(target, internalformat, width, height);
+            }
+        }
+    }
+}
---
+++ SoftGL/RenderContexts/Framebuffer/SoftGLRenderContext..Renderbuffer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        private uint nextRenderbufferName = 0;
+
+        private readonly List<uint> renderbufferNameList = new List<uint>();
+        /// <summary>
+        /// name -> render buffer object.
+        /// </summary>
+        private readonly Dictionary<uint, Renderbuffer> nameRenderbufferDict = new Dictionary<uint, Renderbuffer>();
+
+        private Renderbuffer[] currentRenderbuffers = new Renderbuffer[1];
+        private const int maxRenderbufferSize = 1024 * 8;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="names"></param>
+        public void GenRenderbuffers(int count, uint[] names)
+        {
+            if (count < 0) { SetLastError(ErrorCode.InvalidValue); }
+
+            for (int i = 0; i < count; i++)
+            {
+                uint name = nextRenderbufferName;
+                names[i] = name;
+                renderbufferNameList.Add(name);
+                nextRenderbufferName++;
+            }
+        }
+
+        public void BindRenderbuffer(uint target, uint name)
+        {
+            if (target != GL.GL_RENDERBUFFER) { SetLastError(ErrorCode.InvalidEnum); }
+            if ((name != 0) && (!this.renderbufferNameList.Contains(name))) { SetLastError(ErrorCode.InvalidOperation); }
+
+            Dictionary<uint, Renderbuffer> dict = this.nameRenderbufferDict;
+            if (!dict.ContainsKey(name))
+            {
+                var obj = new Renderbuffer(name);
+                dict.Add(name, obj);
+            }
+
+            this.currentRenderbuffers[target - GL.GL_RENDERBUFFER] = dict[name];
+        }
+
+        public void RenderbufferStorage(uint target, uint internalformat, int width, int height)
+        {
+            if (target != GL.GL_RENDERBUFFER) { SetLastError(ErrorCode.InvalidEnum); }
+            if (width < 0 || maxRenderbufferSize < width) { SetLastError(ErrorCode.InvalidValue); }
+            if (height < 0 || maxRenderbufferSize < height) { SetLastError(ErrorCode.InvalidValue); }
+            // TODO: GL_INVALID_ENUM is generated if internalformat​ is not a color-renderable, depth-renderable, or stencil-renderable format.
+            // TODO: GL_OUT_OF_MEMORY is generated if the GL is unable to create a data store of the requested size.
+
+
+        }
+    }
+}
