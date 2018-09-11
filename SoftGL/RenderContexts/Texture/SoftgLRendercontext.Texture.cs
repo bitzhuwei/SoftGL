a97f364a61576e1de4f592e1c91d8b9f8fa8ecaa
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-11 22:54:51 +0800

    SoftgLRendercontext.Texture

--- SoftGL/RenderContexts/Texture/SoftgLRendercontext.Texture.cs
+++ SoftGL/RenderContexts/Texture/SoftgLRendercontext.Texture.cs
@@ -5,19 +5,6 @@ using System.Text;
 
 namespace SoftGL
 {
-    struct TextureUnit
-    {
-        public uint texture1D;
-        public uint texture2D;
-        public uint texture2DMultisample;
-        public uint texture2DArray;
-        public uint texture3D;
-        public uint texture2DMultisampleArray;
-        public uint textureCubeMap;
-        public uint textureBuffer;
-        public uint textureRectangle;
-    }
-
     partial class SoftGLRenderContext
     {
         private const int GL_MAX_TEXTURE_IMAGE_UNITS = 8;
---
+++ SoftGL/RenderContexts/Utilities/TextureUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftGL
+{
+    struct TextureUnit
+    {
+        public uint texture1D;
+        public uint texture2D;
+        public uint texture2DMultisample;
+        public uint texture2DArray;
+        public uint texture3D;
+        public uint texture2DMultisampleArray;
+        public uint textureCubeMap;
+        public uint textureBuffer;
+        public uint textureRectangle;
+    }
+}
