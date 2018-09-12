06f9fea7aa70f03d9c2d05ea81cf2a83a476e167
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-12 23:43:15 +0800

    glTexParameteri

--- SoftGL/RenderContext/Texture/SampleObject/TexParameters/RC.glTexParameterf.cs
+++ SoftGL/RenderContext/Texture/SampleObject/TexParameters/RC.glTexParameterf.cs
@@ -8,6 +8,9 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
-
+        public static void glTexParameterf(uint target, uint pname, float param)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ SoftGL/RenderContext/Texture/SampleObject/TexParameters/RC.glTexParameteri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        public static void glTexParameteri(uint target, uint pname, int param)
+        {
+            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.TexParameteri(target, pname, param);
+            }
+        }
+
+        private void TexParameteri(uint target, uint pname, int param)
+        {
+
+        }
+    }
+}
