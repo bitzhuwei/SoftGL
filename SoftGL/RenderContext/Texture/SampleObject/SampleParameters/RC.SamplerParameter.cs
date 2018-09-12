0fd61cc744fed6dca6e43f1ef6a931b5375f04f2
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-12 19:44:46 +0800

    add RC. prefix.

---
+++ SoftGL/RenderContext/Texture/SampleObject/SampleParameters/RC.SamplerParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+
+    }
+}
