8c09fd799724b2beb1e306a18a1d04fc2871a19d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-12 10:11:27 +0800

    glGenSamplers

---
+++ OS/SoftOpengl32/Sampler/Sampler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    public partial class StaticCalls
+    {
+        /// <summary>
+        /// generate sampler object names.
+        /// </summary>
+        /// <param name="count">Specifies the number of sampler object names to generate.</param>
+        /// <param name="names">Specifies an array in which the generated sampler object names are stored.</param>
+        public static void glGenSamplers(int count, uint[] names)
+        {
+            SoftGLRenderContext context = StaticCalls.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.GenSamplers(count, names);
+            }
+        }
+
+    }
+}
---
+++ SoftGL/RenderContext/Texture/SampleObject/Sampler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftGL
+{
+    /// <summary>
+    /// Bind a <see cref="Sampler"/> object to a texture uint('s index), and it will affect all textures that bind to the same texture uint.
+    /// </summary>
+    partial class SoftGLRenderContext
+    {
+        private uint nextSamplerName = 0;
+
+        private readonly List<uint> samplerNameList = new List<uint>();
+        /// <summary>
+        /// name -> render buffer object.
+        /// </summary>
+        private readonly Dictionary<uint, Sampler> nameSamplerDict = new Dictionary<uint, Sampler>();
+
+        private Sampler[] currentSamplers = new Sampler[1]; // []
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="names"></param>
+        public void GenSamplers(int count, uint[] names)
+        {
+            if (count < 0) { SetLastError(ErrorCode.InvalidValue); }
+
+            for (int i = 0; i < count; i++)
+            {
+                uint name = nextSamplerName;
+                names[i] = name;
+                samplerNameList.Add(name);
+                nextSamplerName++;
+            }
+        }
+
+    }
+}
