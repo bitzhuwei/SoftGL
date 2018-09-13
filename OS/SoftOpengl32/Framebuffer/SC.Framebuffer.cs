0b1ddd6218e3cf266152807b36cce4fe49fd6e3f
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-13 16:45:37 +0800

    glGenFramebuffers

---
+++ OS/SoftOpengl32/Framebuffer/SC.Framebuffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    partial class StaticCalls
+    {
+        /// <summary>
+        /// generate framebuffer object names.
+        /// </summary>
+        /// <param name="count">Specifies the number of framebuffer object names to generate.</param>
+        /// <param name="names">Specifies an array in which the generated framebuffer object names are stored.</param>
+        public static void glGenFramebuffers(int count, uint[] names)
+        {
+            SoftGLRenderContext.glGenFramebuffers(count, names);
+        }
+    }
+}
--- SoftGL/RenderContext/Framebuffer/RC.Framebuffer.cs
+++ SoftGL/RenderContext/Framebuffer/RC.Framebuffer.cs
@@ -8,5 +8,36 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
+        private uint nextFramebufferName = 0;
+
+        private readonly List<uint> framebufferNameList = new List<uint>();
+        /// <summary>
+        /// name -> render buffer object.
+        /// </summary>
+        private readonly Dictionary<uint, Framebuffer> nameFramebufferDict = new Dictionary<uint, Framebuffer>();
+
+        private Framebuffer currentFramebuffer = null;
+
+        public static void glGenFramebuffers(int count, uint[] names)
+        {
+            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
+            if (context != null)
+            {
+                context.GenFramebuffers(count, names);
+            }
+        }
+
+        private void GenFramebuffers(int count, uint[] names)
+        {
+            if (count < 0) { SetLastError(ErrorCode.InvalidValue); return; }
+
+            for (int i = 0; i < count; i++)
+            {
+                uint name = nextFramebufferName;
+                names[i] = name;
+                framebufferNameList.Add(name);
+                nextFramebufferName++;
+            }
+        }
     }
 }
