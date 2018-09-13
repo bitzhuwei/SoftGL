64fd3b9173213e772dc455700033956212fba091
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-13 22:30:53 +0800

    glCheckFramebufferStatus

---
+++ OS/SoftOpengl32/Framebuffer/SC.Framebuffer.CheckStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    partial class StaticCalls
+    {
+        /// <summary>
+        /// check the completeness status of a framebuffer.
+        /// </summary>
+        /// <param name="target">Specify the target of the framebuffer completeness check.</param>
+        /// <returns></returns>
+        public static uint glCheckFramebufferStatus(uint target)
+        {
+            return SoftGLRenderContext.glCheckFramebufferStatus(target);
+        }
+    }
+}
---
+++ SoftGL/RenderContext/Framebuffer/RC.Framebuffer.CheckStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        public static uint glCheckFramebufferStatus(uint target)
+        {
+            uint result = 0;
+            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
+            if (context != null)
+            {
+                result = context.CheckFramebufferStatus((BindFramebufferTarget)target);
+            }
+
+            return result;
+        }
+
+        private uint CheckFramebufferStatus(BindFramebufferTarget target)
+        {
+            if (target == 0) { SetLastError(ErrorCode.InvalidEnum); return 0; }
+
+            // TODO: check this framebuffer.
+
+            return GL.GL_FRAMEBUFFER_COMPLETE;
+        }
+    }
+}
