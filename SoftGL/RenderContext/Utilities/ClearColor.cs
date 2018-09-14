190a142edb61ca5171ada89e7b7d8fb6a18026b8
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-14 10:33:24 +0800

    Clear

--- SoftGL/RenderContext/Utilities/ClearColor.cs
+++ SoftGL/RenderContext/Utilities/ClearColor.cs
@@ -33,6 +33,14 @@ namespace SoftGL
 
         private void Clear(uint mask)
         {
+            const uint all = (~GL.GL_COLOR_BUFFER_BIT) & (~GL.GL_DEPTH_BUFFER_BIT) & (~GL.GL_STENCIL_BUFFER_BIT);
+            if ((mask & all) != 0) { SetLastError(ErrorCode.InvalidValue); return; }
+
+            bool clearColor = (mask & GL.GL_COLOR_BUFFER_BIT) != 0;
+            bool clearDepth = (mask & GL.GL_DEPTH_BUFFER_BIT) != 0;
+            bool clearStencil = (mask & GL.GL_STENCIL_BUFFER_BIT) != 0;
+            if (!(clearColor || clearDepth || clearStencil)) { return; }
+
 
         }
 
