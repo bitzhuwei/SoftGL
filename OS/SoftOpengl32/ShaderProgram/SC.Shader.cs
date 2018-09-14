7472c256a10e789166322664ae5166e615c33e2f
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-14 19:24:25 +0800

    glCreateShader

---
+++ OS/SoftOpengl32/ShaderProgram/SC.Shader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    public partial class StaticCalls
+    {
+        /// <summary>
+        /// Creates a shader object.
+        /// </summary>
+        /// <param name="shaderType">Specifies the type of shader to be created. Must be one of GL_VERTEX_SHADER, GL_TESS_CONTROL_SHADER, GL_TESS_EVALUATION_SHADER, GL_GEOMETRY_SHADER, GL_FRAGMENT_SHADER, or GL_COMPUTE_SHADER.</param>
+        /// <returns></returns>
+        public static uint glCreateShader(uint shaderType)
+        {
+            return SoftGLRenderContext.glCreateShader(shaderType);
+        }
+    }
+}
---
+++ SoftGL/RenderContext/ShaderProgram/RC.Shader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        private uint nextShaderName = 1;
+
+        /// <summary>
+        /// name -> ShaderProgram object.
+        /// </summary>
+        private readonly Dictionary<uint, Shader> nameShaderDict = new Dictionary<uint, Shader>();
+
+        public static uint glCreateShader(uint shaderType)
+        {
+            uint id = 0;
+            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
+            if (context != null)
+            {
+                id = context.CreateShader((ShaderType)shaderType);
+            }
+
+            return id;
+        }
+
+        private uint CreateShader(ShaderType shaderType)
+        {
+            if (shaderType == 0) { SetLastError(ErrorCode.InvalidEnum); return 0; }
+
+            uint name = nextShaderName;
+            var shader = new Shader(shaderType, name);
+            this.nameShaderDict.Add(name, shader);
+            nextShaderName++;
+
+            return name;
+        }
+    }
+
+    enum ShaderType : uint
+    {
+        VertexShader = GL.GL_VERTEX_SHADER,
+        TessControlShader = GL.GL_TESS_CONTROL_SHADER,
+        TessEvaluationShader = GL.GL_TESS_EVALUATION_SHADER,
+        GeometryShader = GL.GL_GEOMETRY_SHADER,
+        FragmentShader = GL.GL_FRAGMENT_SHADER,
+        ComputeShader = GL.GL_COMPUTE_SHADER
+    }
+}
