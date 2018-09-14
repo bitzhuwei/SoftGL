a57ba772f4d577df498ccd7afce567862b6a06ed
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-14 19:11:06 +0800

    glCreateProgram

---
+++ OS/SoftOpengl32/ShaderProgram/SC.ShaderProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    public partial class StaticCalls
+    {
+        /// <summary>
+        /// Creates a program object.
+        /// </summary>
+        /// <returns></returns>
+        public static uint glCreateProgram()
+        {
+            return SoftGLRenderContext.glCreateProgram();
+        }
+    }
+}
---
+++ SoftGL/RenderContext/ShaderProgram/RC.ShaderProgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftGL
+{
+    partial class SoftGLRenderContext
+    {
+        private uint nextShaderProgramName = 1;
+
+        /// <summary>
+        /// name -> ShaderProgram object.
+        /// </summary>
+        private readonly Dictionary<uint, ShaderProgram> nameShaderProgramDict = new Dictionary<uint, ShaderProgram>();
+
+        private ShaderProgram currentShaderProgram = null;
+
+        public static uint glCreateProgram()
+        {
+            uint id = 0;
+            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
+            if (context != null)
+            {
+                id = context.CreateProgram();
+            }
+
+            return id;
+        }
+
+        private uint CreateProgram()
+        {
+            uint name = nextShaderProgramName;
+            var program = new ShaderProgram(name);
+            this.nameShaderProgramDict.Add(name, program);
+            nextShaderProgramName++;
+
+            return name;
+        }
+    }
+}
