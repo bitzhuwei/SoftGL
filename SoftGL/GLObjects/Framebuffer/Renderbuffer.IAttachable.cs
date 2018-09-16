bfb4a353762989b8c7a5c4bff530f9abccd6c168
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-16 23:21:15 +0800

    IAttachableHelper.Fill

--- SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
+++ SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
@@ -11,25 +11,7 @@ namespace SoftGL
 
         public void Clear(byte[] values)
         {
-            if (values == null || values.Length < 1) { throw new ArgumentNullException("values"); }
-            byte[] dataStore = this.DataStore;
-            int interval = values.Length;
-            int total = dataStore.Length;
-            int tail = total % interval;
-            int i = 0;
-            for (; i + interval < total; i += interval)
-            {
-                for (int j = 0; j < interval; j++)
-                {
-                    dataStore[i + j] = values[j];
-                }
-            }
-            {
-                for (int j = 0; j < tail; j++)
-                {
-                    dataStore[i + j] = values[j];
-                }
-            }
+            IAttachableHelper.Fill(this.DataStore, values);
         }
 
         #endregion IAttachable
