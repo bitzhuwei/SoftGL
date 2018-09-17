c3ad83cf800b70cc0644ff18cd0d30709b5b2f50
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-17 09:58:55 +0800

    glGetUniformLocation

---
+++ OS/SoftOpengl32/ShaderProgram/SC.ShaderProgram.Uniform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    public partial class StaticCalls
+    {
+        /// <summary>
+        /// Returns the location of a uniform variable.
+        /// </summary>
+        /// <param name="program">Specifies the program object to be queried.</param>
+        /// <param name="name">Points to a null terminated string containing the name of the uniform variable whose location is to be queried.</param>
+        /// <returns></returns>
+        public static int glGetUniformLocation(uint program, string name)
+        {
+            return SoftGLRenderContext.glGetUniformLocation(program, name);
+        }
+    }
+}
