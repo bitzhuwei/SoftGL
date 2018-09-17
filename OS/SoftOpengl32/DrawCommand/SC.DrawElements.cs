fb4dceea5a525ec85d52e2f2e27ef50104edc8cb
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-17 19:16:16 +0800

    glDrawElements

---
+++ OS/SoftOpengl32/DrawCommand/SC.DrawElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SoftGL;
+
+namespace SoftOpengl32
+{
+    partial class StaticCalls
+    {
+        /// <summary>
+        /// render primitives from array data.
+        /// </summary>
+        /// <param name="mode">Specifies what kind of primitives to render. Symbolic constants GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP, GL_LINES, GL_LINE_STRIP_ADJACENCY, GL_LINES_ADJACENCY, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES, GL_TRIANGLE_STRIP_ADJACENCY, GL_TRIANGLES_ADJACENCY and GL_PATCHES are accepted.</param>
+        /// <param name="count">Specifies the number of elements to be rendered.</param>
+        /// <param name="type">Specifies the type of the values in indices​. Must be one of GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT, or GL_UNSIGNED_INT.</param>
+        /// <param name="indices">Specifies a byte offset (cast to a pointer type) into the buffer bound to GL_ELEMENT_ARRAY_BUFFER to start reading indices from.</param>
+        public static void glDrawElements(uint mode, int count, uint type, IntPtr indices)
+        {
+            SoftGLRenderContext.glDrawElements(mode, count, type, indices);
+        }
+    }
+}
--- SoftGL/RenderContext/DrawCommand/RC.DrawElements.cs
+++ SoftGL/RenderContext/DrawCommand/RC.DrawElements.cs
@@ -4,18 +4,19 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
-        public static void glSomething(int id)
+        public static void glDrawElements(uint mode, int count, uint type, IntPtr indices)
         {
             SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
             if (context != null)
             {
-                context.Something(id);
+                context.DrawElements(mode, count, type, indices);
             }
         }
 
-        private void Something(int id)
+        private void DrawElements(uint mode, int count, uint type, IntPtr indices)
         {
 
         }
+
     }
 }
