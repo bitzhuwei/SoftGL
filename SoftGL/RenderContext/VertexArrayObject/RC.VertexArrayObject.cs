df03e016152b5db84824f6bf38e9309fada909ca
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-18 16:40:41 +0800

    create default vertex array object

--- SoftGL/RenderContext/VertexArrayObject/RC.VertexArrayObject.cs
+++ SoftGL/RenderContext/VertexArrayObject/RC.VertexArrayObject.cs
@@ -7,7 +7,7 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
-        private uint nextVertexArrayName = 1;
+        private uint nextVertexArrayName = 0;
 
         private readonly List<uint> vertexArrayNameList = new List<uint>();
         /// <summary>
@@ -53,14 +53,11 @@ namespace SoftGL
         {
             if ((name != 0) && (!this.vertexArrayNameList.Contains(name))) { SetLastError(ErrorCode.InvalidOperation); return; }
             VertexArrayObject obj = null;
-            if (name != 0)
+            Dictionary<uint, VertexArrayObject> dict = this.nameVertexArrayDict;
+            if (!dict.TryGetValue(name, out obj)) // create a new texture object.
             {
-                Dictionary<uint, VertexArrayObject> dict = this.nameVertexArrayDict;
-                if (!dict.TryGetValue(name, out obj)) // create a new texture object.
-                {
-                    obj = new VertexArrayObject(name);
-                    dict.Add(name, obj);
-                }
+                obj = new VertexArrayObject(name);
+                dict.Add(name, obj);
             }
 
             this.currentVertexArrayObject = obj;
@@ -99,8 +96,11 @@ namespace SoftGL
             for (int i = 0; i < count; i++)
             {
                 uint name = names[i];
-                if (vertexArrayNameList.Contains(name)) { vertexArrayNameList.Remove(name); }
-                if (nameVertexArrayDict.ContainsKey(name)) { nameVertexArrayDict.Remove(name); }
+                if (name > 0)
+                {
+                    if (vertexArrayNameList.Contains(name)) { vertexArrayNameList.Remove(name); }
+                    if (nameVertexArrayDict.ContainsKey(name)) { nameVertexArrayDict.Remove(name); }
+                }
             }
         }
 
