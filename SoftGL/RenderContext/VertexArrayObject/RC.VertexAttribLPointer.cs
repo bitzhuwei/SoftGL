3cc13602b158462770ae94ddeec8ff4ab43651c1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-18 16:48:59 +0800

    VertexAttrib(I|L)Pointer

--- SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribLPointer.cs
+++ SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribLPointer.cs
@@ -19,7 +19,29 @@ namespace SoftGL
 
         private void VertexAttribLPointer(uint index, int size, VertexAttribLType type, int stride, IntPtr pointer)
         {
-
+            // TODO: GL_INVALID_VALUE is generated if index​ is greater than or equal to GL_MAX_VERTEX_ATTRIBS.
+            if (size != 1 && size != 2 && size != 3 && size != 4) { SetLastError(ErrorCode.InvalidValue); return; }
+            if (type == 0) { SetLastError(ErrorCode.InvalidEnum); return; }
+            if (stride < 0) { SetLastError(ErrorCode.InvalidValue); return; }
+            VertexArrayObject vao = this.currentVertexArrayObject;
+            if (vao == null) { SetLastError(ErrorCode.InvalidOperation); return; }
+            GLBuffer buffer = this.currentBufferDict[BindBufferTarget.ArrayBuffer];
+            // GL_INVALID_OPERATION is generated if zero is bound to the GL_ARRAY_BUFFER buffer object binding point and the pointer​ argument is not NULL.
+            // TODO: why only when "pointer​ argument is not NULL"?
+            if (buffer == null) { SetLastError(ErrorCode.InvalidOperation); return; }
+            VertexAttribDesc desc = null;
+            if (!vao.LocVertexAttribDict.TryGetValue(index, out desc))
+            {
+                desc = new VertexAttribDesc();
+                vao.LocVertexAttribDict.Add(index, desc);
+            }
+            desc.inLocation = index;
+            desc.vbo = buffer;
+            desc.dataSize = size;
+            desc.dataType = (uint)type;
+            //desc.normalize = false; // not needed.
+            desc.startPos = (uint)pointer.ToInt32();
+            desc.stride = (uint)stride;
         }
     }
 
