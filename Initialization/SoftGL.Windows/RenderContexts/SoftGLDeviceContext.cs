cce3c5983d7cadc966cec6a2c6c99caf030feb38
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-25 21:12:37 +0800

    SoftGLDeviceContext

---
+++ Initialization/SoftGL.Windows/RenderContexts/SoftGLDeviceContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftGL
+{
+    class SoftGLDeviceContext : System.Windows.Forms.Control
+    {
+        /// <summary>
+        /// Gets the device context handle.
+        /// </summary>
+        public IntPtr DeviceContextHandle { get { return this.Handle; } }
+    }
+}
