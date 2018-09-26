43417a786dda81ebe5649eda5f34f2de36e41637
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-26 13:19:10 +0800

    DepthRange

--- SoftGL/RenderContext/DrawCommand/DrawElements/LinearInterpolation/RC.Points.cs
+++ SoftGL/RenderContext/DrawCommand/DrawElements/LinearInterpolation/RC.Points.cs
@@ -7,15 +7,17 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
-        private List<Fragment> LinearInterpolationPoints(int count, DrawElementsType type, IntPtr indices, VertexArrayObject vao, ShaderProgram program, GLBuffer indexBuffer, PassBuffer[] passBuffers)
+        private unsafe List<Fragment> LinearInterpolationPoints(int count, DrawElementsType type, IntPtr indices, VertexArrayObject vao, ShaderProgram program, GLBuffer indexBuffer, PassBuffer[] passBuffers)
         {
             var result = new List<Fragment>();
 
+            var gl_PositionArray = (vec4*)passBuffers[0].Mapbuffer().ToPointer();
             byte[] indexData = indexBuffer.Data;
             int byteLength = indexData.Length;
             GCHandle pin = GCHandle.Alloc(indexData, GCHandleType.Pinned);
             IntPtr pointer = pin.AddrOfPinnedObject();
             var gl_VertexIDList = new List<uint>();
+            ivec4 viewport = this.viewport;
             for (int indexID = 0; indexID < count; indexID++)
             {
                 uint gl_VertexID = GetVertexID(pointer, type, indexID);
@@ -23,7 +25,9 @@ namespace SoftGL
                 else { gl_VertexIDList.Add(gl_VertexID); }
 
                 var fragment = new Fragment();
-                //fragment.gl_FragCoord
+                fragment.gl_FragCoord.x = (gl_PositionArray[gl_VertexID].x + 1) / 2.0f * viewport.z + viewport.x;
+                fragment.gl_FragCoord.y = (gl_PositionArray[gl_VertexID].y + 1) / 2.0f * viewport.w + viewport.y;
+                fragment.gl_FragCoord.z = (gl_PositionArray[gl_VertexID].z + 1) / 2.0f * (float)(this.depthRangeFar - this.depthRangeNear) + (float)this.depthRangeNear;
             }
             return result;
         }
