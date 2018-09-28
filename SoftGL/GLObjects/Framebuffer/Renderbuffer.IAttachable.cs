a6e1e57d8bd171b787986df16ce7fcc4877b043d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-28 16:09:50 +0800

    IAttachable.Set

--- SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
+++ SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
@@ -16,7 +16,13 @@ namespace SoftGL
 
         public void Set(int x, int y, byte[] data)
         {
-            throw new NotImplementedException();
+            if (data == null) { return; }
+
+            int singleElementByteLength = this.DataStore.Length / this.Width / this.Height;
+            for (int i = 0; i < singleElementByteLength && i < data.Length; i++)
+            {
+                this.DataStore[(this.Width * y + x) * singleElementByteLength + i] = data[i];
+            }
         }
 
         #endregion IAttachable
