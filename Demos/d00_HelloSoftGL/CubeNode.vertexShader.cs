345a9eb74d1a0cdd22b879e862952ab2d6cd3a02
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-09-30 17:41:18 +0800

    CubeNode

---
+++ Demos/d00_HelloSoftGL/CubeNode.vertexShader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SoftGL;
+
+namespace d00_HelloSoftGL
+{
+    partial class CubeNode
+    {
+        private const string vertexCode = @"using SoftGL;
+
+namespace d00_HelloSoftGL
+{
+    class vertexCode : VertexCodeBase
+    {
+        [In]
+        vec3 inPosition;
+        [Uniform]
+        mat4 mvpMatrix;
+
+        public override void main()
+        {
+            // transform vertex' position from model space to clip space.
+            gl_Position = mvpMatrix * new vec4(inPosition, 1.0f);
+        }
+    }
+}
+";
+    }
+}
