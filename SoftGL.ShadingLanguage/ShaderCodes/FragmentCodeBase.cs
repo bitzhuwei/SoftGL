3cfd068ebe1d0f3037d80438a15907a1c1703021
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert ""discard" in GLSL."
    This reverts commit 5cd2130687c8044089995d22d273691f32edd4a3.

--- SoftGL.ShadingLanguage/ShaderCodes/FragmentCodeBase.cs
+++ SoftGL.ShadingLanguage/ShaderCodes/FragmentCodeBase.cs
@@ -9,11 +9,6 @@ namespace SoftGL
     {
         public vec3 gl_FragCoord;
 
-        /// <summary>
-        /// "discard" in GLSL.
-        /// </summary>
-        public bool discard { get; protected set; }
-
         public abstract void main();
     }
 }
