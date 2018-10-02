fdb56ee5827befaf9750865adfefa676936b0039
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "SoftGLDeviceContext"
    This reverts commit b726c95ba45823a7a4cdf2aa02db6b031acc310d.

--- Initialization/SoftGL.Windows/RenderContexts/SoftGLDeviceContext.cs
+++ Initialization/SoftGL.Windows/RenderContexts/SoftGLDeviceContext.cs
@@ -5,18 +5,11 @@ using System.Text;
 
 namespace SoftGL
 {
-    class SoftGLDeviceContext
+    class SoftGLDeviceContext : System.Windows.Forms.Control
     {
-        System.Windows.Forms.Control control;
         /// <summary>
         /// Gets the device context handle.
         /// </summary>
-        public IntPtr DeviceContextHandle { get { return control.Handle; } }
-
-        public SoftGLDeviceContext(int width, int height)
-        {
-            const int left = 0, top = 0;
-            this.control = new System.Windows.Forms.Control("SoftGLDeviceContext", left, top, width, height);
-        }
+        public IntPtr DeviceContextHandle { get { return this.Handle; } }
     }
 }
