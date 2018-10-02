7e1ba8ce3990db40ac585e324bc0baf2f52660f6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "glDrawElements"
    This reverts commit fb4dceea5a525ec85d52e2f2e27ef50104edc8cb.

--- SoftGL/RenderContext/RC.tmp.cs
+++ SoftGL/RenderContext/RC.tmp.cs
@@ -4,18 +4,5 @@ namespace SoftGL
 {
     partial class SoftGLRenderContext
     {
-        public static void glSomething(int id)
-        {
-            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
-            if (context != null)
-            {
-                context.Something(id);
-            }
-        }
-
-        private void Something(int id)
-        {
-
-        }
     }
 }
