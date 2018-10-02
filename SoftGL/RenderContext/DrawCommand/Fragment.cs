ddfaf378e8b2ed411ffe0784ba6f24c435de4966
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "LinearInterpolationPoints"
    This reverts commit d1ac3b7ca4184064be7d5cdcaee6dfd0f55ffdb6.

--- SoftGL/RenderContext/DrawCommand/Fragment.cs
+++ SoftGL/RenderContext/DrawCommand/Fragment.cs
@@ -5,32 +5,9 @@ using System.Text;
 
 namespace SoftGL
 {
-    unsafe class Fragment
+    class Fragment
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public readonly vec3 gl_FragCoord;
-
-        /// <summary>
-        /// Only contains data in this fragment.
-        /// </summary>
-        public readonly PassBuffer[] attributes;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="fragCoord"></param>
-        /// <param name="attributeLength"></param>
-        public Fragment(vec3 fragCoord, int attributeLength)
-        {
-            this.gl_FragCoord = fragCoord;
-            this.attributes = new PassBuffer[attributeLength];
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}, {1} attributes.", this.gl_FragCoord, this.attributes.Length);
-        }
+        public vec3 gl_FragCoord;
+        public byte[] dataStore;
     }
 }
