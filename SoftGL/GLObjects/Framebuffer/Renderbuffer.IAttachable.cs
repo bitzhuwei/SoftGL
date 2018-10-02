033225bdee12620cc2df4c9a5282281fb3d76d74
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "IAttachable.Set"
    This reverts commit a6e1e57d8bd171b787986df16ce7fcc4877b043d.

--- SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
+++ SoftGL/GLObjects/Framebuffer/Renderbuffer.IAttachable.cs
@@ -16,13 +16,7 @@ namespace SoftGL
 
         public void Set(int x, int y, byte[] data)
         {
-            if (data == null) { return; }
-
-            int singleElementByteLength = this.DataStore.Length / this.Width / this.Height;
-            for (int i = 0; i < singleElementByteLength && i < data.Length; i++)
-            {
-                this.DataStore[(this.Width * y + x) * singleElementByteLength + i] = data[i];
-            }
+            throw new NotImplementedException();
         }
 
         #endregion IAttachable
