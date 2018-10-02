38bd29651cdbe34aad839c0cc5ebfa13cc1a9884
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "glVertexAttribPointer"
    This reverts commit e1312d8130acd311c21aba8e075eaec7fac45ede.

--- SoftGL/GLObjects/VertexArrayObject/VertexArrayObject.cs
+++ SoftGL/GLObjects/VertexArrayObject/VertexArrayObject.cs
@@ -11,12 +11,6 @@ namespace SoftGL
 
         public VertexArrayObject(uint id) { this.Id = id; }
 
-        private Dictionary<uint, VertexAttribDesc> locVertexAttribDict = new Dictionary<uint, VertexAttribDesc>();
-        /// <summary>
-        /// (location = ..) in vec3 inPosition -> glVertexAttrib(I|L)Pointer(..).
-        /// </summary>
-        public Dictionary<uint, VertexAttribDesc> LocVertexAttribDict { get { return locVertexAttribDict; } }
-
         /// <summary>
         ///
         /// </summary>
@@ -25,29 +19,5 @@ namespace SoftGL
         {
             return string.Format("Vertex Array Object: Id:{0}, T:{1}", this.Id);
         }
-    }
-
-    /// <summary>
-    /// glVertexAttrib(I|L)Pointer.
-    /// </summary>
-    class VertexAttribDesc
-    {
-        public uint inLocation;
-        public GLBuffer vbo;
-        public int dataSize;
-        public uint dataType;
-        /// <summary>
-        /// only valid for glVertexAttribPointer(..).
-        /// </summary>
-        public bool normalize;
-        /// <summary>
-        /// pointer.
-        /// </summary>
-        public uint startPos;
-        /// <summary>
-        /// interval.
-        /// </summary>
-        public uint stride;
-        public bool enabled;
     }
 }
--- SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribPointer.cs
+++ SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribPointer.cs
@@ -19,46 +19,7 @@ namespace SoftGL
 
         private void VertexAttribPointer(uint index, int size, VertexAttribType type, bool normalized, int stride, IntPtr pointer)
         {
-            // TODO: GL_INVALID_VALUE is generated if index​ is greater than or equal to GL_MAX_VERTEX_ATTRIBS.
-            if (size != 1 && size != 2 && size != 3 && size != 4 && size != GL.GL_BGRA) { SetLastError(ErrorCode.InvalidValue); return; }
-            if (type == 0) { SetLastError(ErrorCode.InvalidEnum); return; }
-            if (stride < 0) { SetLastError(ErrorCode.InvalidValue); return; }
-            if (size == GL.GL_BGRA)
-            {
-                if (type != VertexAttribType.UnsignedByte
-                    && type != VertexAttribType.Int2101010Rev
-                    && type != VertexAttribType.UnsignedInt2101010Rev)
-                { SetLastError(ErrorCode.InvalidOperation); return; }
-                if (normalized == false)
-                { SetLastError(ErrorCode.InvalidOperation); return; }
-            }
-            if (type == VertexAttribType.Int2101010Rev || type == VertexAttribType.UnsignedInt2101010Rev)
-            {
-                if (size != 4 && size != GL.GL_BGRA) { SetLastError(ErrorCode.InvalidOperation); return; }
-            }
-            if (type == VertexAttribType.UnsignedInt10f11f11fRev)
-            {
-                if (size != 3) { SetLastError(ErrorCode.InvalidOperation); return; }
-            }
-            VertexArrayObject vao = this.currentVertexArrayObject;
-            if (vao == null) { SetLastError(ErrorCode.InvalidOperation); return; }
-            GLBuffer buffer = this.currentBufferDict[BindBufferTarget.ArrayBuffer];
-            // GL_INVALID_OPERATION is generated if zero is bound to the GL_ARRAY_BUFFER buffer object binding point and the pointer​ argument is not NULL.
-            // TODO: why only when "pointer​ argument is not NULL"?
-            if (buffer == null) { SetLastError(ErrorCode.InvalidOperation); return; }
-            VertexAttribDesc desc = null;
-            if (!vao.LocVertexAttribDict.TryGetValue(index, out desc))
-            {
-                desc = new VertexAttribDesc();
-                vao.LocVertexAttribDict.Add(index, desc);
-            }
-            desc.inLocation = index;
-            desc.vbo = buffer;
-            desc.dataSize = size;
-            desc.dataType = (uint)type;
-            desc.normalize = normalized;
-            desc.startPos = (uint)pointer.ToInt32();
-            desc.stride = (uint)stride;
+
         }
     }
 
@@ -73,8 +34,8 @@ namespace SoftGL
         HalfFloat = GL.GL_HALF_FLOAT,
         Float = GL.GL_FLOAT,
         Double = GL.GL_DOUBLE,
-        Fixed = GL.GL_FIXED,
-        Int2101010Rev = GL.GL_INT_2_10_10_10_REV,
+        //Fixed = GL.GL_FIXED, // TODO: to be continued..
+        //Int2101010Rev = GL.GL_INT_2_10_10_10_REV, // TODO: to be continued..
         UnsignedInt2101010Rev = GL.GL_UNSIGNED_INT_2_10_10_10_REV,
         UnsignedInt10f11f11fRev = GL.GL_UNSIGNED_INT_10F_11F_11F_REV
     }
