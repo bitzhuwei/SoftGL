567914b23ad9d76c35b4bfcea92474032d0eb58d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "glUniformMatrix3fv"
    This reverts commit 76afca0404832bcc2c0b3427d413f31fcfc65517.

--- SoftGL/RenderContext/ShaderProgram/RC.ShaderProgram.Uniform.cs
+++ SoftGL/RenderContext/ShaderProgram/RC.ShaderProgram.Uniform.cs
@@ -56,31 +56,5 @@ namespace SoftGL
 
             program.SetUniform4fv(location, count, transpose, value);
         }
-
-        public static void glUniformMatrix3fv(int location, int count, bool transpose, float[] value)
-        {
-            SoftGLRenderContext context = ContextManager.GetCurrentContextObj();
-            if (context != null)
-            {
-                context.UniformMatrix3fv(location, count, transpose, value);
-            }
-        }
-
-        private void UniformMatrix3fv(int location, int count, bool transpose, float[] value)
-        {
-            if (location < 0 || value == null || value.Length != 9) { return; }
-
-            ShaderProgram program = this.currentShaderProgram;
-            if (program == null) { SetLastError(ErrorCode.InvalidOperation); return; }
-            // TODO:GL_INVALID_OPERATION is generated if the size of the uniform variable declared in the shader does not match the size indicated by the glUniform command.
-            if (count < 0) { SetLastError(ErrorCode.InvalidValue); return; }
-
-            UniformVariable v = program.GetUniformVariable(location);
-            if (v == null) { SetLastError(ErrorCode.InvalidOperation); return; }
-            FieldInfo field = v.field;
-            if ((count > 1) && (!field.FieldType.IsArray)) { SetLastError(ErrorCode.InvalidOperation); return; }
-
-            program.SetUniform3fv(location, count, transpose, value);
-        }
     }
 }
