458cd2972acfcb4c39e30d9553fd10a747f95aa3
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "update"
    This reverts commit 946f49b863fd2dd1d13dabf16681490d3cb17568.

--- SoftGL/RenderContext/DrawCommand/DrawElements/LinearInterpolation/RC.LineStrip.cs
+++ SoftGL/RenderContext/DrawCommand/DrawElements/LinearInterpolation/RC.LineStrip.cs
@@ -17,21 +17,21 @@ namespace SoftGL
             {
                 pointers[i] = passBuffers[i + 1].Mapbuffer().ToPointer();
             }
-            byte[] indexData = indexBuffer.Data; int byteLength = indexData.Length;
+            byte[] indexData = indexBuffer.Data;
+            int byteLength = indexData.Length;
             GCHandle pin = GCHandle.Alloc(indexData, GCHandleType.Pinned);
             IntPtr pointer = pin.AddrOfPinnedObject();
             var groupList = new List<LinearInterpolationInfoGroup>();
-            ivec4 viewport = this.viewport;  // ivec4(x, y, width, height)
+            ivec4 viewport = this.viewport;
             for (int indexID = 0; indexID < count - 1; indexID++)
             {
                 var group = new LinearInterpolationInfoGroup(2);
                 for (int i = 0; i < 2; i++)
                 {
                     uint gl_VertexID = GetVertexID(pointer, type, indexID + i);
-                    vec4 gl_Position = gl_PositionArray[gl_VertexID];
-                    vec3 fragCoord = new vec3((gl_Position.x + 1) / 2.0f * viewport.z + viewport.x,
-                    (gl_Position.y + 1) / 2.0f * viewport.w + viewport.y,
-                    (gl_Position.z + 1) / 2.0f * (float)(this.depthRangeFar - this.depthRangeNear) + (float)this.depthRangeNear);
+                    vec3 fragCoord = new vec3((gl_PositionArray[gl_VertexID].x + 1) / 2.0f * viewport.z + viewport.x,
+                    (gl_PositionArray[gl_VertexID].y + 1) / 2.0f * viewport.w + viewport.y,
+                    (gl_PositionArray[gl_VertexID].z + 1) / 2.0f * (float)(this.depthRangeFar - this.depthRangeNear) + (float)this.depthRangeNear);
                     group.array[i] = new LinearInterpolationInfo(indexID + i, gl_VertexID, fragCoord);
                 }
 
