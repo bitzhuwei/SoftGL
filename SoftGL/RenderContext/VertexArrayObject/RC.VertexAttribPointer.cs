4117628e49c772cc2c6d48488e3df9cbc687b7df
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-02 14:49:40 +0800

    Revert "VertexAttribType"
    This reverts commit 067462a82dbdca74e7f42884b35e3688be6ac5d5.

--- SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribPointer.cs
+++ SoftGL/RenderContext/VertexArrayObject/RC.VertexAttribPointer.cs
@@ -70,76 +70,12 @@ namespace SoftGL
         UnsignedShort = GL.GL_UNSIGNED_SHORT,
         Int = GL.GL_INT,
         UnsignedInt = GL.GL_UNSIGNED_INT,
-        /// <summary>
-        /// Half of a float.
-        /// </summary>
         HalfFloat = GL.GL_HALF_FLOAT,
         Float = GL.GL_FLOAT,
         Double = GL.GL_DOUBLE,
-        /// <summary>
-        /// 16:16 int.
-        /// </summary>
         Fixed = GL.GL_FIXED,
         Int2101010Rev = GL.GL_INT_2_10_10_10_REV,
         UnsignedInt2101010Rev = GL.GL_UNSIGNED_INT_2_10_10_10_REV,
         UnsignedInt10f11f11fRev = GL.GL_UNSIGNED_INT_10F_11F_11F_REV
-    }
-
-    static class VertexAttribTypeHelper
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public static Type GetManagedType(this VertexAttribType type)
-        {
-            Type result = null;
-            switch (type)
-            {
-                case VertexAttribType.Byte: result = typeof(byte); break;
-                case VertexAttribType.UnsignedByte: result = typeof(byte); break;
-                case VertexAttribType.Short: result = typeof(short); break;
-                case VertexAttribType.UnsignedShort: result = typeof(ushort); break;
-                case VertexAttribType.Int: result = typeof(int); break;
-                case VertexAttribType.UnsignedInt: result = typeof(uint); break;
-                case VertexAttribType.HalfFloat: result = typeof(ushort); break;
-                case VertexAttribType.Float: result = typeof(float); break;
-                case VertexAttribType.Double: result = typeof(double); break;
-                case VertexAttribType.Fixed: result = typeof(int); break;
-                case VertexAttribType.Int2101010Rev: result = typeof(int); break;
-                case VertexAttribType.UnsignedInt2101010Rev: result = typeof(uint); break;
-                case VertexAttribType.UnsignedInt10f11f11fRev: result = typeof(uint); break;
-                default:
-                    throw new NotDealWithNewEnumItemException(typeof(VertexAttribType));
-            }
-
-            return result;
-        }
-
-        public static uint GetByteLength(this VertexAttribType type)
-        {
-            uint result = 0;
-            switch (type)
-            {
-                case VertexAttribType.Byte: result = sizeof(byte); break;
-                case VertexAttribType.UnsignedByte: result = sizeof(byte); break;
-                case VertexAttribType.Short: result = sizeof(short); break;
-                case VertexAttribType.UnsignedShort: result = sizeof(ushort); break;
-                case VertexAttribType.Int: result = sizeof(int); break;
-                case VertexAttribType.UnsignedInt: result = sizeof(uint); break;
-                case VertexAttribType.HalfFloat: result = sizeof(float) / 2; break;
-                case VertexAttribType.Float: result = sizeof(float); break;
-                case VertexAttribType.Double: result = sizeof(double); break;
-                case VertexAttribType.Fixed: result = sizeof(int); break;
-                case VertexAttribType.Int2101010Rev: result = sizeof(int); break;
-                case VertexAttribType.UnsignedInt2101010Rev: result = sizeof(uint); break;
-                case VertexAttribType.UnsignedInt10f11f11fRev: result = sizeof(uint); break;
-                default:
-                    throw new NotDealWithNewEnumItemException(typeof(VertexAttribType));
-            }
-
-            return result;
-        }
     }
 }
