c531a561e9fa35cc4550cac8af27ee94cf5eddf1
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-07 14:05:08 +0800

    TransformFeedbackCalculator

---
+++ Demos/ShaderStorage/TransformFeedbackCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftGL
+{
+    class TransformFeedbackCalculatorVert : VertexCodeBase
+    {
+        [In]
+        float inValue;
+
+        [Out]
+        float outValue;
+
+        public override void main()
+        {
+            outValue = inValue;
+        }
+    }
+}
