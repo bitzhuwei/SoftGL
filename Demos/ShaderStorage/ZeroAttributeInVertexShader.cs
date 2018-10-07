5b4f5471adc254bbb36fab0a4ce9235d9ec034d6
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-07 13:49:44 +0800

    mvpMat

--- Demos/ShaderStorage/ZeroAttributeInVertexShader.cs
+++ Demos/ShaderStorage/ZeroAttributeInVertexShader.cs
@@ -10,7 +10,7 @@ namespace SoftGL
         [Out]
         vec2 passTexCoord;
         [Uniform]
-        mat4 mvp;
+        mat4 mvpMat;
 
         static readonly vec4[] vertices = new vec4[4] { new vec4(-1.0f, -1.0f, 0.0f, 1.0f), new vec4(1.0f, -1.0f, 0.0f, 1.0f), new vec4(-1.0f, 1.0f, 0.0f, 1.0f), new vec4(1.0f, 1.0f, 0.0f, 1.0f) };
         static readonly vec2[] texCoord = new vec2[4] { new vec2(0.0f, 0.0f), new vec2(1.0f, 0.0f), new vec2(0.0f, 1.0f), new vec2(1.0f, 1.0f) };
@@ -19,16 +19,17 @@ namespace SoftGL
         {
             passTexCoord = texCoord[gl_VertexID];
 
-            gl_Position = mvp * vertices[gl_VertexID];
+            gl_Position = mvpMat * vertices[gl_VertexID];
         }
     }
 
     class ZeroAttributeInVertexShaderFrag : FragmentCodeBase
     {
-        [Uniform]
-        sampler2D tex;
         [In]
         vec2 passTexCoord;
+        [Uniform]
+        sampler2D tex;
+
         [Out]
         vec4 outColor;
 
