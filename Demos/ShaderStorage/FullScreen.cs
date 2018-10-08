97171cc556284939b43c7a90b40de1c2500241df
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2018-10-08 20:53:52 +0800

    static vec2/3/4

---
+++ Demos/ShaderStorage/FullScreen.cs
@@ -0,0 +1,37 @@
+﻿namespace SoftGL
+{
+    class FullScreenVert : VertexCodeBase
+    {
+        [Out]
+        vec2 passTexCoord;
+
+        const float value = 1;
+
+        static readonly vec2[] vertices = new vec2[] { vec2(value, value), vec2(-value, value), vec2(-value, -value), vec2(value, -value) };
+        static readonly vec2[] texCoords = new vec2[] { vec2(1.0, 1.0), vec2(0.0, 1.0), vec2(0.0, 0.0), vec2(1.0, 0.0) };
+
+        public override void main()
+        {
+            gl_Position = vec4(vertices[gl_VertexID], 0, 1);
+
+            passTexCoord = texCoords[gl_VertexID];
+        }
+    }
+
+    class FullScreenFrag : FragmentCodeBase
+    {
+        [In]
+        vec2 passTexCoord;
+
+        [Uniform]
+        sampler2D colorSampler;
+
+        [Out]
+        vec4 outColor;
+
+        public override void main()
+        {
+            outColor = texture(colorSampler, passTexCoord);
+        }
+    }
+}
--- SoftGL.ShadingLanguage/Types/vector.cs
+++ SoftGL.ShadingLanguage/Types/vector.cs
@@ -7,6 +7,57 @@ namespace SoftGL
 {
     partial class CodeBase
     {
+
+        public static vec2 vec2(double x, double y)
+        {
+            return new vec2((float)x, (float)y);
+        }
+
+        public static vec3 vec3(double x, double y, double z)
+        {
+            return new vec3((float)x, (float)y, (float)z);
+        }
+
+        public static vec3 vec3(vec2 xy, double z)
+        {
+            return new vec3(xy.x, xy.y, (float)z);
+        }
+
+        public static vec3 vec3(double x, vec2 yz)
+        {
+            return new vec3((float)x, yz.x, yz.y);
+        }
+
+        public static vec4 vec4(double x, double y, double z, double w)
+        {
+            return new vec4((float)x, (float)y, (float)z, (float)w);
+        }
+
+        public static vec4 vec4(vec2 xy, vec2 zw)
+        {
+            return new vec4(xy.x, xy.y, zw.x, zw.y);
+        }
+
+        public static vec4 vec4(double x, double y, vec2 zw)
+        {
+            return new vec4((float)x, (float)y, zw.x, zw.y);
+        }
+
+        public static vec4 vec4(vec2 xy, double z, double w)
+        {
+            return new vec4(xy.x, xy.y, (float)z, (float)w);
+        }
+
+        public static vec4 vec4(vec3 xyz, double w)
+        {
+            return new vec4(xyz, (float)w);
+        }
+
+        public static vec4 vec4(double x, vec3 yzw)
+        {
+            return new vec4((float)x, yzw.x, yzw.y, yzw.z);
+        }
+
         public static vec3 normalize(vec3 v)
         {
             throw new NotImplementedException();
